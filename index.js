4a725a4159284d2de24d40fce7c65b74f6244ef4
nanjeong <whdwkxj7@naver.com>
2021-11-24 18:47:29 +0900

    상단에 오늘 날짜 표기

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("hello, vanilla.");
+let today = new Date();
+
+let todayDay = today.getDay();
+let todayDate = today.getDate();
+let todayMonth = today.getMonth();
+let todayYear = today.getFullYear();
+
+const dayList = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+const monthList = [
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEP",
+  "OCT",
+  "NOV",
+  "DEC",
+];
+
+let day = document.querySelector("#day");
+let date = document.querySelector("#date");
+let month = document.querySelector("#month");
+let year = document.querySelector("#year");
+
+day.textContent = dayList[todayDay];
+date.textContent = todayDate;
+month.textContent = monthList[todayMonth];
+year.textContent = todayYear;
