53397873edbf69beb25151584893732e22966740
nanjeong <whdwkxj7@naver.com>
2021-11-28 20:36:22 +0900

    리팩토링

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-let today = new Date();
+const today = new Date();
 
-let todayDay = today.getDay();
-let todayDate = today.getDate();
-let todayMonth = today.getMonth();
-let todayYear = today.getFullYear();
+const todayDay = today.getDay();
+const todayDate = today.getDate();
+const todayMonth = today.getMonth();
+const todayYear = today.getFullYear();
+let moveMonth = todayMonth;
+let moveYear = todayYear;
 
 const dayList = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 const monthList = [
@@ -21,21 +23,18 @@ const monthList = [
   "DEC",
 ];
 
-function setDate(setDay, setDate, setMonth, setYear) {
-  let day = document.querySelector("#day");
-  let date = document.querySelector("#date");
-  let month = document.querySelector("#month");
-  let year = document.querySelector("#year");
+const $ = (selector) => document.querySelector(selector);
+const $create = (tagName) => document.createElement(tagName);
+const table = $(".calendar");
 
-  day.textContent = dayList[setDay];
-  date.textContent = setDate;
-  month.textContent = monthList[setMonth];
-  year.textContent = setYear;
+function showDate(setDay, setDate, setMonth, setYear) {
+  $("#day").textContent = dayList[setDay];
+  $("#date").textContent = setDate;
+  $("#month").textContent = monthList[setMonth];
+  $("#year").textContent = setYear;
 }
 
-setDate(todayDay, todayDate, todayMonth, todayYear);
-
-function dayOfTheWeek(year, month, day) {
+function indexOfDay(year, month, date) {
   let days = 0;
 
   for (var y = 1; y < year; y++) {
@@ -46,40 +45,21 @@ function dayOfTheWeek(year, month, day) {
     }
   }
 
-  for (var m = 1; m <= month; m++) {
+  for (var m = 0; m < month; m++) {
     days += daysOftheMonth(year, m);
   }
 
-  days += day;
+  days += date;
 
   return days % 7;
 }
 
 function daysOftheMonth(year, month) {
-  switch (month) {
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12: {
-      return 31;
-    }
-    case 4:
-    case 6:
-    case 9:
-    case 11: {
-      return 30;
-    }
-    default: {
-      if (isLeapYear(year)) {
-        return 29;
-      } else {
-        return 28;
-      }
-    }
+  const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (month === 1 && isLeapYear(year)) {
+    return 29;
   }
+  return days[month];
 }
 
 function isLeapYear(year) {
@@ -90,83 +70,54 @@ function isLeapYear(year) {
 }
 
 function calendar(year, month, today) {
-  var day = dayOfTheWeek(year, month, 1);
-  var date = 1;
-  var daysOfTheMonth = daysOftheMonth(year, month + 1);
+  var idxOfDay = indexOfDay(year, month, 1);
+  var days = 1;
+  var totalDays = daysOftheMonth(year, month);
 
   if (todayYear !== year || todayMonth !== month) {
     today = -1;
   }
 
-  while (date <= daysOfTheMonth) {
-    if (date === 1) {
-      date = appendRow(date, day, 7, today, year, month);
-    } else if (daysOfTheMonth - date < 7) {
-      date = appendRow(date, 0, daysOfTheMonth - date + 1, today, year, month);
+  while (days <= totalDays) {
+    if (days === 1) {
+      days = appendRow(days, idxOfDay, 7, today);
+    } else if (totalDays - days < 7) {
+      days = appendRow(days, 0, totalDays - days + 1, today);
     } else {
-      date = appendRow(date, 0, 7, today, year, month);
+      days = appendRow(days, 0, 7, today);
     }
   }
 }
 
-let table = document.querySelector(".calendar");
-
-function appendRow(date, day, end, today, year, month) {
-  var tr = document.createElement("tr");
-  var tdSun = document.createElement("td");
-  var tdMon = document.createElement("td");
-  var tdTue = document.createElement("td");
-  var tdWed = document.createElement("td");
-  var tdThu = document.createElement("td");
-  var tdFri = document.createElement("td");
-  var tdSat = document.createElement("td");
+function appendRow(date, day, end, today) {
+  var tr = $create("tr");
+  var tdSun = $create("td");
+  var tdMon = $create("td");
+  var tdTue = $create("td");
+  var tdWed = $create("td");
+  var tdThu = $create("td");
+  var tdFri = $create("td");
+  var tdSat = $create("td");
 
   var tdList = [tdSun, tdMon, tdTue, tdWed, tdThu, tdFri, tdSat];
 
   for (var i = day; i < end; i++) {
-    tdList[i].innerHTML = date;
-    tdList[i].addEventListener("click", changeDate(i, date, month, year));
-
+    tdList[i].textContent = date;
     if (today === date) {
       tdList[i].style.color = "red";
     }
     date++;
   }
 
-  tr.appendChild(tdSun);
-  tr.appendChild(tdMon);
-  tr.appendChild(tdTue);
-  tr.appendChild(tdWed);
-  tr.appendChild(tdThu);
-  tr.appendChild(tdFri);
-  tr.appendChild(tdSat);
+  showDate(todayDay, todayDate, todayMonth, todayYear);
+
+  for (let i = 0; i < 7; i++) {
+    tr.appendChild(tdList[i]);
+  }
   table.appendChild(tr);
 
   return date;
 }
-
-function changeDate(day, date, month, year) {
-  return function () {
-    setDate(day, date, month, year);
-  };
-}
-
-calendar(todayYear, todayMonth, todayDate);
-
-let moveMonth = todayMonth;
-let moveYear = todayYear;
-
-let rightIcon = document.querySelector(".icon--right");
-
-rightIcon.addEventListener("click", function () {
-  moveMonth++;
-  if (moveMonth === 12) {
-    moveMonth = 0;
-    moveYear++;
-  }
-
-  resetCalendar(moveYear, moveMonth);
-});
 
 function removeCalendar() {
   var tr = document.querySelectorAll("tr");
@@ -176,9 +127,38 @@ function removeCalendar() {
   }
 }
 
-let leftIcon = document.querySelector(".icon--left");
+function resetCalendar(moveYear, moveMonth) {
+  removeCalendar();
+  calendar(moveYear, moveMonth, todayDate);
 
-leftIcon.addEventListener("click", function () {
+  if (moveYear === todayYear && moveMonth === todayMonth) {
+    showDate(todayDay, todayDate, moveMonth, moveYear);
+  } else {
+    showDate(indexOfDay(moveYear, moveMonth, 1), 1, moveMonth, moveYear);
+  }
+}
+
+table.addEventListener("click", (e) => {
+  if (e.target.tagName === "TD" && e.target.textContent !== "") {
+    const date = parseInt(e.target.textContent);
+    const month = monthList.indexOf($("#month").textContent);
+    const year = parseInt($("#year").textContent);
+    const day = indexOfDay(year, month, date);
+    showDate(day, date, month, year);
+  }
+});
+
+$(".icon--right").addEventListener("click", function () {
+  moveMonth++;
+  if (moveMonth === 12) {
+    moveMonth = 0;
+    moveYear++;
+  }
+
+  resetCalendar(moveYear, moveMonth);
+});
+
+$(".icon--left").addEventListener("click", function () {
   moveMonth--;
   if (moveMonth < 0) {
     moveMonth = 11;
@@ -188,13 +168,4 @@ leftIcon.addEventListener("click", function () {
   resetCalendar(moveYear, moveMonth);
 });
 
-function resetCalendar(moveYear, moveMonth) {
-  removeCalendar();
-  calendar(moveYear, moveMonth, todayDate);
-
-  if (moveYear === todayYear && moveMonth === todayMonth) {
-    setDate(todayDay, todayDate, moveMonth, moveYear);
-  } else {
-    setDate(dayOfTheWeek(moveYear, moveMonth, 1), 1, moveMonth, moveYear);
-  }
-}
+calendar(todayYear, todayMonth, todayDate);
