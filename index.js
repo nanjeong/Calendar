48a47ae9766f35205d84ff9630966d15c1a54fea
nanjeong <whdwkxj7@naver.com>
2021-12-01 17:33:22 +0900

    추가되는 리스트 아이템 스타일링

--- index.js
+++ index.js
@@ -147,7 +147,9 @@ $(".icon--left").addEventListener("click", function () {
 
 $(".icon--plus").addEventListener("click", function () {
   let schedule = prompt("일정을 입력해주세요", "예) 12일 친구 생일");
-  const template = `<li>${schedule}</li>`;
+  const template = `<li class="schedule-item"><input class="schedule-item-checkbox" type="checkbox">${schedule}</input><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 icon icon--trash" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+</svg></li>`;
   if (schedule) {
     $(".schedule-list").insertAdjacentHTML("beforeend", template);
   }
